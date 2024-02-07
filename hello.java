58a1b73824abefcb3456e578f8a176cd4d301fbd
jyotikulkarnisachin <153583818+jyotikulkarnisachin@users.noreply.github.com>
2024-02-07 09:06:12 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,8 +2,8 @@ public class hello
 {
 public static void main(String[] args) 
 { 
-System.out.println("****");
+System.out.println("*******************=====================**************************");
 System.out.println("Hello from GITHub"); 
-System.out.println("*****");
+System.out.println("*******************=====================**************************");
 }
 }
